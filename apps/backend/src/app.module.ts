7473a48e09a0fc6c1874c1faf29a58d2ea74c9c3
Joshua Grant <grantjoshua1995@gmail.com>
2025-12-06 22:20:33 +0100

    fix main nest.js module to pass in user-games module.  remove proxy.

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,10 +4,16 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { BacklogsModule } from "./backlogs/backlogs.module";
 import { GamesModule } from "./games/games.module";
+import { UserGamesModule } from "./user-games/user-games.module";
 
 @Module({
 	controllers: [AppController],
 	providers: [AppService],
-	imports: [GamesModule, ConfigModule.forRoot(), BacklogsModule],
+	imports: [
+		GamesModule,
+		ConfigModule.forRoot(),
+		BacklogsModule,
+		UserGamesModule,
+	],
 })
 export class AppModule {}
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -16,13 +16,6 @@ const config = defineConfig({
 	],
 	server: {
 		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:3001",
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ""),
-			},
-		},
 	},
 });
 
