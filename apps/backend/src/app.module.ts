664c17ff1767cbbc7404ef482262e450b11f8306
Joshua Grant <grantjoshua1995@gmail.com>
2025-12-10 21:26:58 +0100

    remove example app..controller

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { APP_GUARD } from "@nestjs/core";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+
 import { ApiKeyGuard, AuthModule } from "./auth";
 import { DatabaseModule } from "./database";
 import { GamesModule } from "./games/games.module";
@@ -16,9 +15,7 @@ import { UserGamesModule } from "./user-games/user-games.module";
 		GamesModule,
 		UserGamesModule,
 	],
-	controllers: [AppController],
 	providers: [
-		AppService,
 		{
 			provide: APP_GUARD,
 			useClass: ApiKeyGuard,
